60fcecc6e411b7bd3d813f573738f2b5baca655c
Phenethylamine <phenethylamine@tutanota.com>
2018-05-07 15:29:57 -0400

    Remove unreusable traces of authable

--- mix.exs
+++ mix.exs
@@ -40,8 +40,7 @@ defmodule Fontina.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:authable, "~> 0.9.1"}
+      {:cowboy, "~> 1.0"}
     ]
   end
 
