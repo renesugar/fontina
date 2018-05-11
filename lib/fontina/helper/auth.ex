c3be5c4696e3617a875ba7704ebdd847e0c16e35
Phenethylamine <phenethylamine@tutanota.com>
2018-05-11 13:20:56 -0400

    Remove debug bits

--- lib/fontina/helper/auth.ex
+++ lib/fontina/helper/auth.ex
@@ -8,12 +8,9 @@ defmodule Fontina.Helper.Auth do
     do: :ok
 
   def authorize_for_resource(conn) do
-    IO.puts "here"
-    IO.inspect conn
     with token when not is_nil(token) <- get_session(conn, :session_token),
          %Token{user_id: user_id} <- Repo.get_by(Token, value: token),
          %User{} = user <- Repo.get!(User, user_id) do
-      IO.puts "success"
       {:ok, user}
     else
       _e -> nil
